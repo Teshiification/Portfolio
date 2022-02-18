ee84d14e265224bf98c95873c498deeb2769d4a3
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-18 16:03:21 +0100

    Create acomplishments.jsx

---
+++ src/constants/acomplishments.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { DiStackoverflow, DiReact, DiGithub } from 'react-icons/di';
+import instagram from '../scripts/instagram';
+
+export const AcomplishmentsData = [
+    { icon: <DiStackoverflow size="3rem"/>, number: (new Date().getFullYear()-2010), text: 'Jahre am programmieren', },
+    { icon: <DiReact size="3rem"/>, number: 10000, text: 'Instagram Follower', },
+    { icon: <DiGithub size="3rem"/>, number: 0, text: 'GitHub Follower :(', },
+  ];
