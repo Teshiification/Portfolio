138625dc08a855b6c8f8ef991b63c0120a9b927a
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-18 15:57:59 +0100

    Update Technologies.jsx

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,49 +1,38 @@
 import React from 'react';
-import { DiFirebase, DiPhotoshop, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { GridContainer, ListContainer, ListTitle, TagList, ListIcon, Tag } from './TechnologiesStyles'
+import { TechnologiesData } from '../../constants/technologies';
+import { Section, GradientText, BlogCard, H1 } from '../../themes/global';
+import FadeInSection from '../../animations/FadeInSection';
 
-const Technologies = () =>  (
-  <Section id ="tech">
-    <SectionDivider/>
-    <br/>
-    <SectionTitle>Technologien</SectionTitle>
-    <SectionText>
-    Ich habe mit einer Reihe von Technologien in der Webentwicklung gearbeitet<br/>
-    </SectionText>
-    <List>
-      <ListItem>
-        <DiReact size="3rem"/>
-        <ListContainer>
-          <ListTitle>Front-End</ListTitle>
-          <ListParagraph>
-            Erfahrungen mit <br/>
-            React und php
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <DiFirebase size="3rem"/>
-        <ListContainer>
-          <ListTitle>Back-End</ListTitle>
-          <ListParagraph>
-          Erfahrungen mit  <br/>
-            Java, c#, c++, SQL
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <DiPhotoshop size="3rem"/>
-        <ListContainer>
-          <ListTitle>UI/UX</ListTitle>
-          <ListParagraph>
-          Erfahrungen mit  <br/>
-            Adobe und Figma
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-    </List>
-  </Section>
+const Technologies = () => (
+    <Section id="technologies">
+        <H1>Skills</H1>
+        <GridContainer>
+            {TechnologiesData.map((card,i)=>(
+                <FadeInSection>
+            <BlogCard key={card.id}>
+                <ListIcon src='/logo512.png'>
+                    {card.icon}
+                </ListIcon>
+                <ListContainer>
+                    <ListTitle>
+                        <GradientText>
+                            {card.title}
+                        </GradientText>
+                    </ListTitle>
+                    <TagList>
+                        {card.tags.map((tag,i)=>(                            
+                            <GradientText>
+                                <Tag key={i}>{tag}</Tag>
+                            </GradientText>
+                        ))}
+                    </TagList>
+                </ListContainer>
+            </BlogCard>
+            </FadeInSection>
+            ))}
+        </GridContainer>
+    </Section>
 );
 
 export default Technologies;
