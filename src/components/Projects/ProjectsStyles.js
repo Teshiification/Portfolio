a1b00e6c9c1a179a9fbc660ffa41d0f126f52359
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-18 15:57:30 +0100

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,112 +1,78 @@
 import styled from 'styled-components';
 
+export const GridContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+  place-items: center;
+  align-items: center;
+  column-gap: 2rem;
+  row-gap: 2rem;
+  max-height: 75vh;
+  overflow-y: scroll;
+  scroll-snap-type: y mandatory;
+  padding-top: 1rem;
+  background: var(--color-bg-darkened);
+  box-shadow: 0px 0px 1rem var(--color-bg-darkened);  
+  border-radius: 20px;
+  margin: 1rem;
+`
+
 export const Img = styled.img`
-  width:100%;
-  height:100%;
+  width: 100%;
   object-fit: cover;
   overflow: hidden;
+  border-radius: 10px 10px 0 0;
 `
 
-export const GridContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-padding: 3rem;
-place-items: center;
-column-gap: 2rem;
-row-gap: 3rem;
-@media ${(props) => props.theme.breakpoints.sm} {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  padding-bottom: 0;
-}
+//TITLE:
 
+export const TitleContent = styled.p`
+font-weight: 800;
+  text-align: center;
+  width: 100%;
+  font-size: clamp(1.5rem,5vw,1rem);
 `
-export const BlogCard = styled.div`
-  border-radius: 10px;
-  box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
-  text-align: center;
-  width: 400px;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    width: 100%;
-  }
-`;
-export const TitleContent = styled.div`
-  text-align: center;
-  z-index: 20;
+
+export const CardInfo = styled.div`
   width: 100%;
-
-`;
-
-
-export const HeaderThree = styled.h3`
-  font-weight: 500;
-  letter-spacing: 2px;
-  color: #9cc9e3;
-  padding: .5rem 0;
-  font-size: ${(props) => props.title ? '3rem' : '2rem'};
-`;
-
-export const Hr = styled.hr`
-  width: 50px;
-  height: 3px;
-  margin: 20px auto;
-  border: 0;
-  background: #d0bb57;
-`;
-
-export const Intro = styled.div`
-  width: 170px;
-  margin: 0 auto;
-  color: #dce3e7;
-  font-family: 'Droid Serif', serif;
-  font-size: 13px;
-  font-style: italic;
-  line-height: 18px;
-`;
-
-
-export const CardInfo = styled.p`
-  width: 100%;
-  padding: 0 50px;
-  color: #e4e6e7;
-  font-style: 2rem;
-  line-height: 24px;
-  text-align: justify;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    padding:.3rem
-  
-}
-`;
-
+  max-height: 1vh;
+  text-align: center;
+  color: var(--color-text);
+`
 
 export const UtilityList = styled.ul`
   list-style-type: none;
-  padding: 0;
+  padding: 1rem;
   display: flex;
   justify-content: space-around;
   margin: 2.5rem 0;
-`;
+`
 
-export const ExternalLinks = styled.a`
-color:#d4c0c0;
-font-size: 1.6rem;
-padding:1rem 1.5rem;
-background: #6b3030;
-border-radius: 15px;
-transition: 0.5s;
-&:hover{
-  background: #801414;
-
-}
-`;
+//Tags:
 
 export const TagList = styled.ul`
-display: flex;
-justify-content: space-around;
-padding: 2rem;
+  display: flex;
+  justify-content: space-around;
+  padding: .5rem;
 `
+
 export const Tag = styled.li`
-color: #d8bfbf;
-font-size: 1.5rem;
+`
+
+//Divider:
+
+export const HrSmall = styled.hr`
+  width: 4rem;
+  height: .2rem;
+  margin: 20px auto;
+  border: none;
+  background: var(--color-text);
+`
+
+export const HrBig = styled.hr`
+  width: 90%;
+  height: 3px;
+  margin: 20px auto;
+  border: none;
+  background: var(--color-text);
 `
