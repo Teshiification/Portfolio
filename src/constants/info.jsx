b40e7527e44747006ad89d2e503a2cbb345b5e37
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-18 16:03:05 +0100

    Create info.jsx

---
+++ src/constants/info.jsx
@@ -0,0 +1,8 @@
+export const InfoData = [
+    {
+        forname:"Danny",
+        surname:"Sinicco",
+        title:"Portfolio",
+        phrase:'"Das Portfolio ist eine zielgerichtete Sammlung von Arbeiten, welche die Anstrengungen des Lernenden, den Lernfortschritt und die Leistungsresultate auf einem oder mehreren Gebieten zeigt."',
+    }
+];
