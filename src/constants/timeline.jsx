cad8f26dbe64f060563888d89113fa092ec2d989
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-18 16:02:36 +0100

    Create timeline.jsx

---
+++ src/constants/timeline.jsx
@@ -0,0 +1,10 @@
+export const TimelineData = [
+    { year: 2010, text: 'Erster Kontakt mit der Programmierung (Visual Basic)', },
+    { year: 2011, text: 'HTML & PHP', },
+    { year: 2012, text: 'Datenbanken (mySQL)', },
+    { year: 2015, text: 'Unity Engine (C#)', },
+    { year: 2017, text: 'Fachabitur: Wirtschaftsinformatik (Visual Basic, JAVA, mySQL)', },
+    { year: 2018, text: 'JavaScript & Bootstrap', },
+    { year: 2021, text: 'Ausbildungsstart als Anwendungsentwickler (C++)', },
+    { year: 2022, text: 'REACT', },
+  ];
