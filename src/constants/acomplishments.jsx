450d5a781c7399d4ddc23eb08099c78d6cc6f2fd
Danny <54819795+Teshiification@users.noreply.github.com>
2022-02-22 13:03:43 +0100

    Update acomplishments.jsx

--- src/constants/acomplishments.jsx
+++ src/constants/acomplishments.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { DiStackoverflow, DiReact, DiGithub } from 'react-icons/di';
-import instagram from '../scripts/instagram';
 
 export const AcomplishmentsData = [
     { icon: <DiStackoverflow size="3rem"/>, number: (new Date().getFullYear()-2010), text: 'Jahre am programmieren', },
